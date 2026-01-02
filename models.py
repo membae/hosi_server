99066e2a5325307baf060cb324badea9f912a184
membae <membaphilip1@gmail.com>
2026-01-03 02:57:57 +0300

    added patient model

--- models.py
+++ models.py
@@ -16,4 +16,15 @@ class User(db.Model, SerializerMixin):
     password=db.Column(db.String, nullable=False)
     role=db.Column(db.String, default="Admin", nullable=False)
     
-            
+class Patient(db.Model, SerializerMixin):
+    __tablename__="patients"
+    id=db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String, nullable=False)
+    last_name=db.Column(db.String, nullable=False)
+    phone_number=db.Column(db.String, unique=True, nullable=False)  
+    status=db.Column(db.String, default="Admitted", nullable=False)
+    doctor_summary=db.Column(db.String, nullable=False)
+    diagnosis=db.Column(db.String, nullable=False)
+    admitted_at=db.Column(db.DateTime, nullable=True)
+    discharged_at=db.Column(db.DateTime, nullable=True)
+              
